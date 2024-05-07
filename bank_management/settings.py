cfa3bed15e5dbba7b6119e71460745ea1a88563e
muz2002 <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2024-05-08 04:29:33 +0600

    .

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-p_x#ce#l&9gpu(*dhf$1t8%m0safan95)*)qd22a8mc#2bl!f6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

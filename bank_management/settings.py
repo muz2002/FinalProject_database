bd36e8105b1d14845532f197b1248102de3ee192
muz2002 <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2024-05-09 04:31:32 +0600

    .

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p_x#ce#l&9gpu(*dhf$1t8%m0safan95)*)qd22a8mc#2bl!f6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
